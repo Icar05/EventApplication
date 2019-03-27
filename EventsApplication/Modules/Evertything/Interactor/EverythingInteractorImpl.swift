858d17864f626357fd37d7d7c04c44a25da42363
iPinguin <galiatkin.o@gmail.com>
2019-03-27 10:27:26 +0200

    add interactions

---
+++ EventsApplication/Modules/Evertything/Interactor/EverythingInteractorImpl.swift
@@ -0,0 +1,13 @@
+//
+//  EverythingInteractorImpl.swift
+//  EventsApplication
+//
+//  Created by Eddson on 3/26/19.
+//  Copyright © 2019 ipinguin_linuxoid. All rights reserved.
+//
+
+import Foundation
+
+class EverythingInteractorImpl: EvertythingInteractor {
+    
+}
---
+++ EventsApplication/Modules/Headers/Interactor/HeaderInteractorImpl.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderInteractorImpl.swift
+//  EventsApplication
+//
+//  Created by Eddson on 3/26/19.
+//  Copyright © 2019 ipinguin_linuxoid. All rights reserved.
+//
+
+import Foundation
+
+class HeaderInteractorImpl: HeaderInteractor {
+    
+}
