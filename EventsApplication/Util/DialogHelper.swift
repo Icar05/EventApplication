85a902c14bd03ad9a8ded2d8a879bfd9770b23c9
Icar05 <galiatkin.o@gmail.com>
2019-03-28 03:14:11 +0200

    add first table view

---
+++ EventsApplication/Util/DialogHelper.swift
@@ -0,0 +1,21 @@
+//
+//  DialogHelper.swift
+//  EventsApplication
+//
+//  Created by Ipinguin_linuxoid on 2019/03/28.
+//  Copyright © 2019 ipinguin_linuxoid. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DialogHelper {
+    
+    static func presentErrorDialog(error: Error, viewController: UIViewController){
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription,
+                                      preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+        viewController.present(alert, animated: true, completion: nil)
+    }
+    
+}
