d90071a4fbe3d9010ba673500095129fbb51780a
iPinguin <galiatkin.o@gmail.com>
2019-04-01 15:48:04 +0300

    create search dialog layout

---
+++ EventsApplication/custom/dialogs/searchDialog/UISearchDialog.swift
@@ -0,0 +1,14 @@
+//
+//  UISearchDialog.swift
+//  EventsApplication
+//
+//  Created by Eddson on 4/1/19.
+//  Copyright © 2019 ipinguin_linuxoid. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UISearchDialog: UIViewController{
+    
+}
