efd84acb7827759ce3ad9d4f7ac6fb9193f625e2
Icar05 <galiatkin.o@gmail.com>
2019-04-15 22:10:47 +0300

    prepare repo

---
+++ EventsApplication/Repository/Repository.swift
@@ -0,0 +1,30 @@
+//
+//  Repository.swift
+//  EventsApplication
+//
+//  Created by Ipinguin_linuxoid on 4/15/19.
+//  Copyright © 2019 ipinguin_linuxoid. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol Repository{
+    
+    func saveArticles(articles: [Articles]) -> Observable<Bool>
+    
+    func saveSources(articles: [Sources]) -> Observable<Bool>
+    
+    func getHeadlines(country: String) -> Observable<[Articles]>
+    
+    func getHeadlines(category: String) -> Observable<[Articles]>
+    
+    func getHeadlines() -> Observable<[Articles]>
+    
+    func getEverything(query: String) -> Observable<[Articles]>
+    
+    func getEverything(query: String, language: String) -> Observable<[Articles]>
+    
+    func getSourcesByCategory(category: String) -> Observable<[Sources]>
+    
+}
---
+++ EventsApplication/Repository/RepositoryImpl.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryImpl.swift
+//  EventsApplication
+//
+//  Created by Ipinguin_linuxoid on 4/15/19.
+//  Copyright © 2019 ipinguin_linuxoid. All rights reserved.
+//
+
+import Foundation
+
+class RepositoryImpl: Repository {
+    
+}
